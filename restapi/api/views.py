0d82632f320621302c3c797dd5d850263ecd1fff
Nikhil Joy <nikh.liberty77@gmail.com>
2021-01-21 16:04:51 +0400

    django api done for register

---
+++ restapi/api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .serializers import UserRegister
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class register(APIView):
+
+    def post(self,request,format=None):
+        serializer=UserRegister(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account=serializer.save()
+            data['response']='registered'
+            data['username']=account.username
+            data['email']=account.email
+            token,create=Token.objects.get_or_create(user=account)
+            data['token']=token.key
+        else:
+            data=serializer.errors
+        return Response(data)
+
